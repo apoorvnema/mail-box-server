d79ecb91d0d479e51e4f52bb7677879c28ef7b58
apoorvnema <apoorvnema38@gmail.com>
2024-09-22 01:36:35 +0530

    [UPDATE]: Inbox View

--- controllers/mail.js
+++ controllers/mail.js
@@ -25,5 +25,40 @@ exports.send = async (req, res) => {
 };
 
 exports.inbox = async (req, res) => {
+    try {
+        const mails = await Mail.find({ to: req.user._id });
+        const senderIds = mails.map(mail => mail.from);
+        const senders = await User.find({ _id: { $in: senderIds } });
+        const senderMap = {};
+        senders.forEach(sender => {
+            senderMap[sender._id] = sender.email;
+        });
+        const mailsWithSenders = mails.map(mail => ({
+            ...mail.toObject(),
+            sender: senderMap[mail.from]
+        }));
+        res.status(200).json(mailsWithSenders);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
 
-}
+exports.mailDetails = async (req, res) => {
+    try {
+        const mail = await Mail.findOne({ _id: req.params.id, to: req.user._id });
+        if (!mail) {
+            return res.status(404).json({ message: 'Mail not found' });
+        }
+        const sender = await User.findById(mail.from);
+        if (!sender) {
+            return res.status(404).json({ message: 'Sender not found' });
+        }
+        const mailWithSender = {
+            ...mail.toObject(),
+            sender: sender.email
+        };
+        res.status(200).json(mailWithSender);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
--- routes/mail.js
+++ routes/mail.js
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/send', authenticate, userController.send);
 router.get('/inbox', authenticate, userController.inbox);
+router.get('/inbox/:id', authenticate, userController.mailDetails);
 
 module.exports = router;
